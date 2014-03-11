d84b296282ecfdde8868e6c738afcb3a5febabe5
markcsilverman <markcsilverman@gmail.com>
2014-03-10 21:31:15 -0400

    Test Change
    Testing Collaboration

--- TestMVC/TestMVC/Models/Movie.cs
+++ TestMVC/TestMVC/Models/Movie.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-
+// Testing a change to code.
 namespace TestMVC.Models
 {
     public class Movie
